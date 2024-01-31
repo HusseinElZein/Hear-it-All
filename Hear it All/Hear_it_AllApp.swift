a52bc7b3e6ad8d3865a17d92e29093d8021f1fe0
Hussein El-Zein <91071020+HusseinElZein@users.noreply.github.com>
2024-01-31 09:53:04 +0100

    Initial Commit

---
+++ Hear it All/Hear_it_AllApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hear_it_AllApp.swift
+//  Hear it All
+//
+//  Created by Hussein on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hear_it_AllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
